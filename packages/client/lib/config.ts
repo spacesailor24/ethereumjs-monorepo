a098920db6f415ce92e2a86e7d233c1a93d4dff8
Jochem Brouwer <jochembrouwer96@gmail.com>
2021-01-21 21:42:21 +0100

    client: save private rlpx key to disk

--- packages/client/lib/config.ts
+++ packages/client/lib/config.ts
@@ -282,4 +282,11 @@ export class Config {
     if (option !== undefined) return option
     return this.chainCommon.chainName() === 'mainnet'
   }
+  
+  getNetworkDir(): string {
+    const networkDirName = this.common.chainName()
+    const dataDir = `${this.datadir}/${networkDirName}`
+
+    return dataDir
+  }
 }
